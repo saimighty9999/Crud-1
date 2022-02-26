63b5f3153d4f7d8c1882e01decfbf3689fe46d11
saimighty9999 <saimighty9999@gmail.com>
2022-02-26 16:09:32 +0530

    msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomertableComponent } from './customertable/customertable.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { TabledataService } from './service/Tabledata.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomertableComponent,
+    CustomerDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
-  providers: [],
+  providers: [TabledataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
